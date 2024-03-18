018257494ed2ccfe583ae8ca035aa39502d80e98
eehanseul <gkstmf616@gmail.com>
2024-03-18 09:48:48 +0900

    feat: add mecab

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-from crawling.crawling import total_crawling
+from konlpy.tag import Mecab
+from collections import Counter
+from crawling.crawling import total_crawling, each_crawling
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,10 +10,26 @@ def hello_world():
 
 @app.route('/api/news', methods=['POST'])
 def crawling_keyword():
-    stockList = request.json['stockList'] # [code]
-    stock_contents = total_crawling(stockList)
-    # TODO stock_contents 키워드 뽑기
-    return stock_contents
+    code = request.json['stockList'] # [code]
+    # stock_contents = total_crawling(stockList)
+    # # TODO stock_contents 키워드 뽑기
+    news_text = each_crawling(code[0]); # code에 대한 뉴스 가져옴
+    #return news_text
+
+    # KoNLpy + Mecab : 형태소 분석
+    # 형태소 분석기로 명사만 추출,1글자는 의미없다고 보고 삭제
+    path = "/opt/homebrew/lib/mecab/dic/mecab-ko-dic"
+    try:
+        mecab = Mecab() 
+    except: 
+        mecab = Mecab(path)
+    nouns = mecab.nouns(news_text)
+    nouns = [n for n in nouns if len(n) > 1]
+
+    # 단어 개수 세기, 가장 많이 등장한 N개 구하기(Counter.most_common())
+    count = Counter(nouns)
+    tags = count.most_common(40)
+    print(tags[:20])
 
 if __name__ == '__main__':
     app.run(debug=True)
